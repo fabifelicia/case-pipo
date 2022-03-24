51c49f8f60ffb03901fe92f8ae6cabad18e0f7b4
fabifelicia <fabiana.menezes@dcomp.ufs.br>
2022-03-24 15:11:40 -0300

    add partner repository

---
+++ src/repositories/partner.repository.js
@@ -0,0 +1,67 @@
+import db from '../db.js';
+
+class PartnerRepository {
+    async findAllPartners() {
+        const query = `
+            SELECT uuid, name
+            FROM application_partner        
+        `
+
+        const { rows } = await db.query(query);
+
+        return rows || []
+    }
+
+    async findPartnerById(uuid) {
+        const query = `
+            SELECT uuid, name
+            FROM application_partner
+            WHERE uuid = $1        
+        `
+        const values = [uuid]
+        const { rows } = await db.query(query, values);
+
+        const [partner] = rows
+
+        return partner
+    }
+
+    async create(partner) {
+        const script = `
+            INSERT INTO application_partner (name)
+            VALUES($1)
+            RETURNING uuid                  
+        `
+        const values = [partner.name]
+        const { rows } = await db.query(script, values);
+        
+        const [newPartner] = rows
+
+        return newPartner.uuid
+    }
+
+    async update(partner) {
+        const script = `
+            UPDATE application_partner
+            SET
+                name = $1            
+            WHERE uuid = $2            
+        `
+        const values = [partner.name, partner.uuid]
+        await db.query(script, values)  
+    }
+
+    async remove(uuid) {
+        const script = `
+            DELETE 
+            FROM application_partner
+            WHERE uuid = $1
+        `
+        const values = [uuid]
+
+        await db.query(script, values) 
+    }
+
+}
+
+export default new PartnerRepository()
