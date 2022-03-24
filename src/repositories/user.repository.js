5ad74ef6d3e5f6605d77245864cf7f22a9b2522a
fabifelicia <fabiana.menezes@dcomp.ufs.br>
2022-03-24 20:52:15 -0300

    add user repository

---
+++ src/repositories/user.repository.js
@@ -0,0 +1,67 @@
+import db from '../db.js';
+
+class UserRepository {
+    async findAllUsers() {
+        const query = `
+            SELECT uuid, name
+            FROM users        
+        `
+
+        const { rows } = await db.query(query);
+
+        return rows || []
+    }
+
+    async findUserById(cpf) {
+        const query = `
+            SELECT cpf, name
+            FROM users
+            WHERE cpf = $1        
+        `
+        const values = [cpf]
+        const { rows } = await db.query(query, values);
+
+        const [user] = rows
+
+        return user
+    }
+
+    async create(user) {
+        const script = `
+            INSERT INTO users (name)
+            VALUES($1)
+            RETURNING cpf, name                  
+        `
+        const values = [user.name]
+        const { rows } = await db.query(script, values);
+        
+        const [newUser] = rows
+
+        return newUser
+    }
+
+    async update(user) {
+        const script = `
+            UPDATE users
+            SET
+                name = $1            
+            WHERE cpf = $2            
+        `
+        const values = [user.name, user.cpf]
+        await db.query(script, values)  
+    }
+
+    async remove(cpf) {
+        const script = `
+            DELETE 
+            FROM users
+            WHERE cpf = $1
+        `
+        const values = [cpf]
+
+        await db.query(script, values) 
+    }
+
+}
+
+export default new UserRepository()
